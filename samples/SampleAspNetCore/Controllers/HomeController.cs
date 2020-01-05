a42f44e8f9926cd4bbd2d0cf252724052ed98a80
Johnny Oshika <johnnyoshika@gmail.com>
2020-01-05 11:03:25 -0800

    Add index page

---
+++ samples/SampleAspNetCore/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleAspNetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => Content("Hello world!");
+    }
+}
--- samples/SampleAspNetCore/Startup.cs
+++ samples/SampleAspNetCore/Startup.cs
@@ -16,6 +16,7 @@ namespace SampleWeb
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,13 +29,7 @@ namespace SampleWeb
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            app.UseEndpoints(endpoints => endpoints.MapDefaultControllerRoute());
         }
     }
 }
